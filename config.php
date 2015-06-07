e0ab9b2bfb27b7d6d3c4aa7cb50b287a5ff22c65
Zenta Hori <zhori@coiney.com>
2015-06-07 21:48:29 +0900

    Update de settings

--- config.php
+++ config.php
@@ -6,22 +6,22 @@
 define('MULTISITE', 0);
 
 /** MySQL database name for Osclass */
-define('DB_NAME', 'osclass');
+define('DB_NAME', 'd5tft796gego1k');
 
 /** MySQL database username */
-define('DB_USER', 'menatoz');
+define('DB_USER', 'nfrioippqztvfr');
 
 /** MySQL database password */
-define('DB_PASSWORD', '0L9aYPp0');
+define('DB_PASSWORD', 'pcWv2hG_ZmqjnloY672tGs_Z29');
 
 /** MySQL hostname */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'ec2-204-236-228-77.compute-1.amazonaws.com');
 
 /** Database Table prefix */
 define('DB_TABLE_PREFIX', 'oc_');
 
 define('REL_WEB_URL', '/osclass/');
 
-define('WEB_PATH', 'http://localhost/osclass/');
+define('WEB_PATH', 'https://spider-net.herokuapp.com/');
 
 ?>
