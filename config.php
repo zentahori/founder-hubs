6860c5889245aabb59048ae116873dd529e334d8
Zenta Hori <zhori@coiney.com>
2015-06-10 00:47:53 +0900

    changed config.php

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * The base MySQL settings of Osclass
+ */
+define('MULTISITE', 0);
+
+/** MySQL database name for Osclass */
+define('DB_NAME', 'heroku_fd6d6848392cda3');
+
+/** MySQL database username */
+define('DB_USER', 'b3e21e1b897559');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'b74655d3');
+
+/** MySQL hostname */
+define('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net');
+
+/** Database Table prefix */
+define('DB_TABLE_PREFIX', 'oc_');
+
+define('REL_WEB_URL', 'rel_here');
+
+define('WEB_PATH', 'https://spider-net.herokuapp.com/');
+
+?>
