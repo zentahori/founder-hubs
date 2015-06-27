862c9a8ace676af6133f4fa555865443f76209c0
Zenta Hori <zhori@coiney.com>
2015-06-27 23:59:08 +0900

    translate comments into Japanese

--- oc-content/plugins/youtube/item_form.php
+++ oc-content/plugins/youtube/item_form.php
@@ -1,9 +1,9 @@
 <h2><?php _e('Youtube video', 'youtube'); ?></h2>
 <div class="box">
     <div class="row">
-        <?php printf( __( 'Enter the youtube url, i.e.: <em>%s</em> or <em>%s</em>', 'youtube' ), 'http://www.youtube.com/watch?v=ojqWclLQOxk', 'http://www.youtube.com/v/ojqWclLQOxk') ; ?>
+        <?php printf( __( 'Youtubeのurlを入力してください。<br> 例: <em>%s</em> または <br> <em>%s</em>', 'youtube' ), 'http://www.youtube.com/watch?v=ojqWclLQOxk', 'http://www.youtube.com/v/ojqWclLQOxk') ; ?>
     </div>
-    <div class="row" style="width: 500px;">
+    <div class="row" style="width: 500px; text-align:center">
         <input type="text" name="s_youtube" value="<?php echo $detail['s_youtube'] ; ?>" />
     </div>
 </div>
