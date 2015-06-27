a3205b2748aab0954b2ae32952fceffc450d632a
Zenta Hori <zhori@coiney.com>
2015-06-28 00:05:52 +0900

    correct alignment of text

--- oc-content/plugins/youtube/item_form.php
+++ oc-content/plugins/youtube/item_form.php
@@ -1,9 +1,14 @@
+<br>
 <h2><?php _e('Youtube video', 'youtube'); ?></h2>
 <div class="box">
     <div class="row">
         <?php printf( __( 'Youtubeのurlを入力してください。<br> 例: <em>%s</em> または <br> <em>%s</em>', 'youtube' ), 'http://www.youtube.com/watch?v=ojqWclLQOxk', 'http://www.youtube.com/v/ojqWclLQOxk') ; ?>
     </div>
+<!--
     <div class="row" style="width: 500px; text-align:center">
+-->
+    <div class="row" style="text-align:center">
         <input type="text" name="s_youtube" value="<?php echo $detail['s_youtube'] ; ?>" />
     </div>
 </div>
+<br>
