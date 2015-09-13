e8c98b7982a50b80c1476be5e5de169226ee80f9
Zenta Hori <zhori@coiney.com>
2015-09-13 22:15:30 +0900

    add a production server in config

--- config.php
+++ config.php
@@ -7,6 +7,7 @@ define('MULTISITE', 0);
 
 if ($_SERVER['SERVER_ADDR'] == '127.0.0.1'
         || $_SERVER['SERVER_ADDR'] == '::1') {  // Development
+
     /** MySQL database name for Osclass */
     define('DB_NAME', 'osclass');
 
@@ -25,7 +26,30 @@ if ($_SERVER['SERVER_ADDR'] == '127.0.0.1'
     define('REL_WEB_URL', '/founder-hubs/');
 
     define('WEB_PATH', 'http://localhost/founder-hubs/');
-} else { // Production
+
+} else if ($_SERVER['HTTP_HOST'] == "ciao-spider-net.ssl-lolipop.jp") { // Production
+
+    /** MySQL database name for Osclass */
+    define('DB_NAME', 'LAA0658931-osclass');
+
+    /** MySQL database username */
+    define('DB_USER', 'LAA0658931');
+
+    /** MySQL database password */
+    define('DB_PASSWORD', '********');
+
+    /** MySQL hostname */
+    define('DB_HOST', 'mysql101.phy.lolipop.lan');
+
+    /** Database Table prefix */
+    define('DB_TABLE_PREFIX', 'oc_');
+
+    define('REL_WEB_URL', '');
+
+    define('WEB_PATH', 'https://ciao-spider-net.ssl-lolipop.jp/');
+
+} else { // Staging
+
     /** MySQL database name for Osclass */
     define('DB_NAME', 'heroku_fd6d6848392cda3');
 
@@ -44,6 +68,7 @@ if ($_SERVER['SERVER_ADDR'] == '127.0.0.1'
     define('REL_WEB_URL', '/osclass/');
 
     define('WEB_PATH', 'https://spider-net.herokuapp.com/');
+
 }
 
 ?>
